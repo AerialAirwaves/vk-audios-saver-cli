2aedc234d6ba9d3034ce2976b9ab37a7fe483440
MelnikovSM <melnikovsm2@gmail.com>
2016-03-18 16:26:23 +0500

    20160318

--- sync.py
+++ sync.py
@@ -40,13 +40,22 @@ def get_audio(token, uid):
 print "Типо самопальная качалка музончега с ВКонтактика by MelnikovSM"
 if not os.path.exists(config.MUSIC_PATH):
 	os.makedirs(config.MUSIC_PATH)
-print "Гружу список музона со страницы.."
+print "Гружу список аудиозаписей со страницы.."
 token, user_id = get_token(['audio'])
 user_audio = get_audio(token, user_id)['response']
-print "Качаю музон в заданный в конфиге каталог: %s" % (config.MUSIC_PATH)
+print "Качаю аудиозаписи в заданный в конфиге каталог: %s" % (config.MUSIC_PATH)
 i = 0
 ei = 0
 afi = 0
+
+flist=[f for f in os.listdir(config.MUSIC_PATH) if os.path.isfile(os.path.join(config.MUSIC_PATH, f))]
+
+def diff(a, b):
+	b = set(b)
+	return [aa for aa in a if aa not in b]
+
+rlist=[]
+
 fp=codecs.open(config.MUSIC_PATH+"/"+config.PLAYLIST, 'w', 'utf-8')
 fp.write("#EXTM3U\n")
 for track in user_audio:
@@ -81,7 +90,7 @@ for track in user_audio:
 					afi+=1
 			except AttributeError:
 				nic=2
-				print "Какая-то хрень с именем файла, пофиг, сохраню как "+aid+".mp3"
+				print "Какая-то хрень с именем файла. Пофиг, сохраню как "+aid+".mp3"
 				filepath = os.path.join(config.MUSIC_PATH, "%s.mp3" % (aid))
 				g = urlgrabber.grabber.URLGrabber(reget='simple')
 				if os.path.isfile(filepath)==False:
@@ -104,15 +113,35 @@ for track in user_audio:
 	if nic==1:
 		fp.write("#EXTINF:,%s\n" % (artist+" - "+title))
 		fp.write("%s.mp3\n" % (fname))
+		rlist.append("%s.mp3" % (fname))
 	elif nic==2: 
 		fp.write("#EXTINF:,%s\n" % (artist+" - "+title))
 		fp.write("%s.mp3\n" % (aid))
+		rlist.append("%s.mp3" % (aid))
 fp.close()
+rlist.append(config.PLAYLIST)
 print """>>>>> Закачка завершена! <<<<<
 ==========> Статистика работы <=========="""
-print "Всего музла на странице: %s" %(i)
+print "Всего аудиозаписей на странице: %s" %(i)
 if afi>0:
-	print "Ранее скачаного музла в каталоге: %s" %(afi)
+	print "Ранее скачаных аудиозаписей в каталоге: %s" %(afi)
 print "Всего скачано/докачано: %s" %(i-afi-ei)
 if ei>0:
 	print "Ошибок закачки: %s" %(ei)
+
+diffr=diff(flist, rlist)
+if len(diffr)>0:
+	diffm=[]
+	difff=[]
+	for name in diffr:
+		if ".mp3" in name:
+			diffm.append(name)
+		else:
+			difff.append(name)
+	if len(diffm)>0:
+		print "Найдено %s (возможно) лишних аудиозаписей:" %(len(diffm))
+		for name in diffm: print name
+		print "Возможно они были удалены одминами ВКонтакта по требованию копирастов, или вы просто удалили эти аудиозаписи со страницы. Что с ними делать, решать вам."
+	if len(difff)>0:
+		print "Найдено %s (возможно) лишних файлов:" % len(difff)
+		for name in difff: print name
