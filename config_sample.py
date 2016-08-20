3b54ce9b1ad8af9e909b65c179d21e47a729b184
MelnikovSM <melnikovsm2@gmail.com>
2016-08-21 01:45:18 +0600

    20160820

--- config_sample.py
+++ config_sample.py
@@ -8,7 +8,11 @@
 # https://oauth.vk.com/authorize?client_id=3697615&scope=audio,offline&response_type=token
 # Then copy access_token value from address link and put into token value below
 # P.S. Read this instruction for more correct understanding: http://zenno.pro/kak-poluchit-access-token-prilozheniya-vk-com/
-# Notice: You should use any offical apps APP ID, that allows to get token (Windows 8-10 PC/Mobile app ID is set by default, but servers don't allow to get token from Android APP ID, for example). In other case VK servers won't give all your audios in resoponse of this script request.
+
+# Notice: You should use any offical apps APP ID, that allows to get token (Windows 8-10 PC/Mobile app ID is set by default, but servers don't allow to get token from Android APP ID, for example).
+# In other case VK servers won't give all your audios in resoponse of this script request.
+# Real experience at 20 Aug 2016: from my 822 audios BEFORE it returned for download 498-500 audios only, but AFTER it return 818 (where it lost last 4 I still cannot understand).
+# This solution offered by Elisey Izumtsev. Huge human thanks to him.
 
 # VK Access Information
 
